520e886257ffd31ace60d0922aeb199ef671104d
Fulal-kt <muhamedfulail77@gmail.com>
2024-05-11 14:59:14 +0530

    alternative added in Image tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 <div className="details">
                     <h1>CURRENCY</h1>
                     <div className="usa">
-                    <Image src={png?.usa}/><h1>• &nbsp;USA</h1>
+                    <Image src={png?.usa} alt="usa"/><h1>• &nbsp;USA</h1>
                     </div>
                     <p className="currencyNotes">Transactions will be completed in Euros and a currency reference is available on hover.</p>
                 </div>
@@ -36,7 +36,7 @@ const Footer = () => {
             <div className="bottomFooter-firstOne">
                 <div className="headingText">
                     <h1>mettā muse</h1>
-                <div className="downArrow"><span> <Image src={png.arrow}/></span></div>
+                <div className="downArrow"><span> <Image src={png.arrow} alt="downArrow"/></span></div>
 
                 </div>
                 <ul>
@@ -49,7 +49,7 @@ const Footer = () => {
             <div className="bottomFooter-secondOne">
                 <div className="headingText">
                     <h1>QUICK LINKS</h1>
-                    <div className="downArrow"><span> <Image src={png.arrow}/></span></div>
+                    <div className="downArrow"><span> <Image src={png.arrow} alt="Arrow"/></span></div>
                 </div>
                 <ul>
                     {footerNavQuick.map((link,index)=>(
@@ -64,14 +64,14 @@ const Footer = () => {
                     <h1>FOLLOW US</h1>
                     <div className="downArrow"><span> <Image src={png.arrow}/></span></div>
                 </div>
-                    <div className="socialMedia"> <Image src={png?.insta} width={30} height={30}/> <Image src={png?.linkedin} width={30} height={30}/> </div>
+                    <div className="socialMedia"> <Image src={png?.insta} width={30} alt="instagram" height={30}/> <Image src={png?.linkedin}alt="linkedin" width={30} height={30}/> </div>
                     
                 </div>
                 <div className="subDiv">
               
                     <h1>mettā muse Accepts</h1>
                   
-                <div> <Image src={png?.pay1}/> <Image src={png?.pay2}/> <Image src={png?.pay3}/> <Image src={png?.pay4}/> <Image src={png?.pay5}/> <Image src={png?.pay6}/> </div>
+                <div> <Image src={png?.pay1} alt="pay1"/> <Image src={png?.pay2} alt="pay2"/> <Image alt="pay3"  src={png?.pay3}/> <Image alt="pay4" src={png?.pay4}/> <Image src={png?.pay5} alt="pay5"/> <Image src={png?.pay6} alt="pay6"/> </div>
                 </div>
             </div>
           </div>
