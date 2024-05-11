e81e2f97c04305ccc742095f5ac819651339e75b
Fulal-kt <muhamedfulail77@gmail.com>
2024-05-11 13:34:45 +0530

    almost completed

---
+++ src/constants/constants.js
@@ -0,0 +1,48 @@
+import arrow from '../../public/images/arrow.png'
+import heart from '../../public/images/heart.png'
+
+
+import pay1 from '../../public/images/pay1.png'
+import pay2 from '../../public/images/pay2.png'
+import pay3 from '../../public/images/pay3.png'
+import pay4 from '../../public/images/pay4.png'
+import pay5 from '../../public/images/pay5.png'
+import pay6 from '../../public/images/pay6.png'
+import insta from '../../public/images/insta.png'
+import linkedin from '../../public/images/linkedin.png'
+import usa from '../../public/images/usa.png'
+
+
+export const png={arrow,heart,pay1,pay2,pay3,pay4,pay5,pay6,linkedin,insta,usa}
+
+
+
+
+export const navbarHeadings = [
+    { title: "SHOP", link: "#" },
+    { title: "SKILLS", link: "#" },
+    { title: "STORIES", link: "#" },
+    { title: "ABOUT", link: "#" },
+    { title: "CONTACT US", link: "#" },
+];
+
+export const footerNavMeta = [
+    { title: "About Us", link: "#" },
+    { title: "Stories", link: "#" },
+    { title: "Artisans", link: "#" },
+    { title: "Boutiques", link: "#" },
+    { title: "Contact Us", link: "#" },
+    { title: "EU Compliances Docs", link: "#" }
+];
+export const footerNavQuick = [
+    { title: "Order & Shipping", link: "#" },
+    { title: "Join/Login as a Seller", link: "#" },
+    { title: "Payment & Pricing", link: "#" },
+    { title: "Return & Refunds", link: "#" },
+    { title: "FAQs", link: "#" },
+    { title: "Privacy Policy", link: "#" },
+    { title: "Terms & Conditions", link: "#" }
+];
+
+
+
